57fc297959db4bf28f396eb4a4d1256e78cce716
Tim <timsmith@ou.edu>
2016-12-01 13:55:24 -0600

    fixing comments

--- src/SearchGateway/Model/PrimoSilo.php
+++ src/SearchGateway/Model/PrimoSilo.php
@@ -20,6 +20,7 @@ Class PrimoSilo extends Silo {
    */
   public function getResult($query, $limit) {
 
+    #only set this is it is for 'books only'
     $bookSearchArg = ($this->primoBook) ? ',AND&pfilter=pfilter,exact,books,AND&mode=advanced' : '';
 
     $myResult = new Result();
@@ -33,13 +34,15 @@ Class PrimoSilo extends Silo {
     # https://developers.exlibrisgroup.com/primo/apis/webservices/rest/pnxs
     $primoRequest = $this->client->createRequest('GET', $this->primoHost . "/primo/v1/pnxs");
     $primoQuery = $primoRequest->getQuery();
-    $primoQuery['q'] = 'any,contains,' . $query;
-    $primoQuery['limit'] = $limit;
-    $primoQuery['apikey'] = $this->primoKey;
-    $primoQuery['vid'] = $this->vid;
-    $primoQuery['scope'] = 'default_scope';
-    $primoQuery['addfields'] = ['pnxId'];
-    $primoQuery['view'] = 'full';
+    $primoQuery['q'] = 'any,contains,' . $query; //the search term
+    $primoQuery['limit'] = $limit; //number of records to return
+    $primoQuery['apikey'] = $this->primoKey; //private key for OU
+    $primoQuery['vid'] = $this->vid; //identification code...right now it is 'OUNEW'
+    $primoQuery['scope'] = 'default_scope'; //range of types to return...default is everything
+    $primoQuery['addfields'] = ['pnxId']; //specific identifier for individual records
+    $primoQuery['view'] = 'full'; //view = full will return everything...including the subject or description
+
+    #if this is 'books only' then we need to set that facet type
     if ($this->primoBook) {
       $primoQuery['qInclude'] = 'facet_rtype,exact,books';
     }
@@ -51,11 +54,13 @@ Class PrimoSilo extends Silo {
 
 	# Process hits
 	foreach ($primoJson['docs'] as $docs) {
+        #if there is more than one subject it is returned as an array
+        #if it is we separate each one by a vertical bar '|'
         $implodedSubs = (is_array($docs['subject'])) ? implode(' | ', $docs['subject']) : $docs['subject'];
 
 	    $my_title = $docs['title'];
 	    $my_link  = "https://ou-primo.hosted.exlibrisgroup.com/primo-explore/fulldisplay?docid=".$docs['pnxId']."&vid=".$this->vid."";
-	    $my_description  = $implodedSubs; // no good source for description known
+	    $my_description  = $implodedSubs;
 
 	    $myResult->addHit($my_link, $my_title, $my_description);
 	}
